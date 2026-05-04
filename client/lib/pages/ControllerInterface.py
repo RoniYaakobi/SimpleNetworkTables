03cc498acf77999c3ec7fc3cd0d63012cc4819eb
Roni <roniyaakobi@yahoo.com>
2026-05-04 11:51:34 +0300

    about to check the new build.py

--- client/lib/pages/ControllerInterface.py
+++ client/lib/pages/ControllerInterface.py
@@ -1,19 +1,20 @@
+from __future__ import annotations
 __author__ = "RONI YAAKOBI"
+
 import tkinter as tk
-from typing import Any
+from typing import Any,Type
 
-from client.src.Backend import AppBackend
+
 from client.lib.commands.CommandScheduler import CommandScheduler
-
 
 class ControllerInterface:
     """ A class that acts as an interface for all objects that can trigger a command, or can send you to a page which can """
-    def __init__(self, backend: Any, scheduler: CommandScheduler, controller: None | "ControllerInterface" = None):
+    def __init__(self, backend: Any, scheduler: CommandScheduler, controller: None | ControllerInterface = None):
         """
         Args:
             backend (Any): the backend for the application. Use this to communicate between the frontend and the backend
             scheduler (CommandScheduler): The command scheduler that this controller is using to schedule commands.
-            controller (None | ControllerInterface, optional): _description_. Defaults to None.
+            controller (None | ControllerInterface, optional): The controller which controlls the controller. Defaults to None.
         """
         self.m_backend = backend
         self.m_scheduler = scheduler
@@ -21,13 +22,26 @@ class ControllerInterface:
         self.m_controller = controller
         self.m_fields = []
 
-    def backend(self) -> AppBackend:
+    def backend(self) :
+        """ Get this controller's backend. """
         return self.m_backend
     
     def scheduler(self) -> CommandScheduler:
+        """
+        Returns:
+            CommandScheduler: The command scheduler which the controller is using.
+        """
         return self.m_scheduler
     
     def get_page(self, page_type):
+        """Search for a Page by id or class. Checks in the current controller, and parent controllers, otherwise returns None
+
+        Args:
+            page_type (Page | int): The page id or class.
+
+        Returns:
+            Page: the page of that type.
+        """
         if not isinstance(page_type, int):
             page_type = page_type.PAGE_ID
 
@@ -41,7 +55,12 @@ class ControllerInterface:
             return None
 
 
-    def goto(self, page_type):
+    def goto(self, page_type: type | int):
+        """ Uses `get_page()` to get the page of a type, then displays it after reseting fields
+
+        Args:
+            page_type (Page | int): the page type you to be shown
+        """
         page = self.get_page(page_type)
         if page:
             for field in self.m_fields:
--- client/lib/pages/page.py
+++ client/lib/pages/page.py
@@ -1,50 +1,100 @@
+from __future__ import annotations
 __author__ = "RONI YAAKOBI"
 import tkinter as tk
 from tkinter import messagebox
 from client.lib.commands.Command import Command
 
-from client.lib.pages.ControllerInterface import ControllerInterface
+from .ControllerInterface import ControllerInterface
+
+from typing import Callable, Any
 
 
 class PageType:
+    """Stores the page names to id maps
+    """
     ids_to_names = dict()
     names_to_ids = dict()
     page_types = 0
 
     @staticmethod
-    def assign_id(name):
+    def assign_id(name : str):
+        """Assign a page an id by registering it to the PageType maps
+
+        Args:
+            name (str): a string identifier for the page
+
+        Returns:
+            int: amount of pages registered including this one, which is the id.
+        """
         PageType.page_types += 1
         PageType.ids_to_names = {PageType.page_types: name, **PageType.ids_to_names}
         PageType.names_to_ids = {name: PageType.page_types, **PageType.names_to_ids}
         return PageType.page_types
 
     @staticmethod
-    def identify(name):
+    def identify(name: str):
+        """
+        Args:
+            name (str): the string identifier used to register the page with `assign_id()`
+
+        Returns:
+            int: Page id of the respective page.
+        """
         return PageType.names_to_ids.get(name)
 
 
 class Page(tk.Frame, ControllerInterface):
+    """ A generic page with utilities """
+
     PAGE_ID = PageType.assign_id("plain")
 
     def __init__(self, parent, controller):
+        """ A weird way to do inheritance in python because python is weird. """
         tk.Frame.__init__(self, parent)
         ControllerInterface.__init__(self, controller.backend(), controller.scheduler(), controller= controller)
 
     def show(self):
+        """ Show this page """
         self.tkraise()
         self.m_controller.current_page = __class__.PAGE_ID
 
     def set_title(self, text="Title", font=("Arial", 20)):
+        """ Set the title of the form.
+
+        Args:
+            text (str, optional): Defaults to "Title".
+            font (tuple, optional): (Font style, Font size). Defaults to ("Arial", 20).
+        """
         tk.Label(self, text=text, font=font).pack(pady=10)
 
     def add_radio_choice(self, text="Option", value=0, variable=None):
+        """A radio choice for the form
+
+        Args:
+            text (str, optional): The text of the option. Defaults to "Option".
+            value (int, optional): The value of the option. Defaults to 0.
+            variable (_type_, optional): The variable that is shared between the different radio options. Defaults to None.
+
+        Returns:
+            tk.IntVar: If no varibale is passed, auto initialize a new int variable, and use that for the radio.
+        """
         if variable is None:
             variable = tk.IntVar(value=value)
         tk.Radiobutton(self, text=text, value=value, variable=variable).pack()
 
         return variable
 
-    def create_text_box(self, name, update_command, getter, pack_padx=0, pack_pady=0):
+    def create_text_box(self, name: str, update_command: Callable[[object],None], getter: Callable[[object],Any],
+                         pack_padx=0, pack_pady=0):
+        """Create a text box. There is a default text box style. TODO move this out of lib
+
+        Args:
+            name (str): What is the title above the text box
+            update_command (Callable[[object],None]): How to update the display.
+            getter (Callable[[object],Any]): What to update the display to
+            pack_padx (int, optional): Padding on the x axis on packing. Defaults to 0.
+            pack_pady (int, optional): Padding on the y axis on packing. Defaults to 0.
+        """
         text_box_style = {
             "bg":"white",
             "fg":"black",
@@ -72,17 +122,39 @@ class Page(tk.Frame, ControllerInterface):
 
 
     def create_field(self, text="field", hidden= False):
+        """Build a field for user entry
+
+        Args:
+            text (str, optional): What is the title of the field. Defaults to "field".
+            hidden (bool, optional): Whether or not the field is hidden (like a password). Defaults to False.
+
+        Returns:
+            tk.Entry: the field to extract the input from.
+        """
         tk.Label(self, text=text).pack()
         field = tk.Entry(self, show="*") if hidden else tk.Entry(self)
         self.m_fields.append(field)
         return field
     
-    def create_action_button(self, text="Action",
-        command= lambda: messagebox.showerror("Action button","No action"), pack_pady=0):
+    def create_action_button(self, text: str = "Action",
+        command: Callable[[object],None] = lambda: messagebox.showerror("Action button","No action"), pack_pady=0):
+        """Create a button that does something on activation.
+
+        Args:
+            text (str, optional): The caption of the button. Defaults to "Action".
+            command (Callable[[object],None], optional): _description_. Defaults to lambda:messagebox.showerror("Action button","No action").
+            pack_pady (int, optional): How much vertical padding to give the button. Defaults to 0.
+        """
         
         tk.Button(self, text= text, command= command).pack(pady=pack_pady)
 
-    def add_link(self, page_type: type , text="Go to a page", pack_pady=0):
+    def add_link(self, page_type: type , text: str = "Go to a page", pack_pady=0):
+        """ Add a link to the page
+        Args:
+            page_type (Page): The page class that we want to link to.
+            text (str, optional): The link text. Defaults to "Go to a page".
+            pack_pady (int, optional): Vertical padding of the link in the page. Defaults to 0.
+        """
         self.m_links[page_type] = self
         self.create_action_button(text=text,
                                    command= lambda: self.goto(page_type), pack_pady=pack_pady)
